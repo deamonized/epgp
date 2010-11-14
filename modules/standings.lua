b39852c17b6e9281437b369163568ce35d8227a3
evlogimenos <devnull@localhost>
2010-11-14 17:42:56 +0100

    Move standings into modules and clean up the interface a bit.

---
+++ modules/standings.lua
@@ -0,0 +1,109 @@
+local mod = EPGP:NewModule("standings", "AceEvent-3.0")
+
+local Debug = LibStub("LibDebug-1.0")
+local L = LibStub("AceLocale-3.0"):GetLocale("EPGP")
+
+mod.dbDefaults = {
+  profile = {
+    enabled = true,
+    sort_order = "PR",
+    show_offline = true,
+    show_only_raid = true,
+  }
+}
+
+local standings
+
+function mod:ShouldShow(i)
+  local  name, rank, rankIndex, level, class, zone, note,
+  officernote, online, status, classFileName,
+  achievementPoints, achievementRank, isMobile = GetGuildRosterInfo(i)
+  if not self.db.profile.show_offline and not online then return false end
+  if self.db.profile.show_only_raid and UnitInRaid("player") and not UnitInRaid(name) then return false end
+  local info = EPGP:GetMemberInfo(name)
+  if info and info.GetEP() then return true end
+  print(name)
+end
+
+local function BuildStandings(self)
+  standings = {}
+  local totalMembers = GetNumGuildMembers()
+  for i = 1, totalMembers do
+    if self:ShouldShow(i) then
+      local name = GetGuildRosterInfo(i)
+      local info = EPGP:GetMemberInfo(name)
+      -- Show all mains that have ep.
+      table.insert(standings,
+                   {info.GetClass(), info.GetName(),
+                    info.GetEP(), info.GetGP(), info.GetPR()})
+    end
+  end
+  EPGP:SortStandings(EPGP:StandingsSortOrder())
+end
+mod.GUILD_ROSTER_UPDATE = EPGP.Profile(
+  BuildStandings, "Updating standings (GUILD_ROSTER_UPDATE)")
+mod.BaseGPChanged = EPGP.Profile(
+  BuildStandings, "Updating standings (BaseGPChanged)")
+
+local function BuildComparator(...)
+  local chunks = {}
+  table.insert(chunks, "return function(a, b) local x, y")
+  for i = 1, select('#', ...) do
+    local chunk = [[
+        x, y = ]] .. select(i, ...) .. [[
+        if x ~= y then
+          return x < y
+        end
+    ]]
+    table.insert(chunks, chunk)
+  end
+  table.insert(chunks, "end")
+  local code = table.concat(chunks, "\n")
+  return assert(loadstring(code))()
+end
+
+local NAME_SEL = "a[2], b[2]"
+local EP_SEL = "b[3], a[3]"
+local GP_SEL = "b[4], a[4]"
+local PR_SEL = "b[5], a[5]"
+local MIN_EP_SEL =
+  "b[3] < EPGP.db.profile.min_ep, a[3] < EPGP.db.profile.min_ep"
+
+local comparator = {
+  NAME = BuildComparator(NAME_SEL),
+  EP = BuildComparator(EP_SEL, NAME_SEL),
+  GP = BuildComparator(GP_SEL, NAME_SEL),
+  PR = BuildComparator(MIN_EP_SEL, PR_SEL, NAME_SEL),
+}
+
+function mod:OnModuleEnable()
+  self:RegisterEvent("GUILD_ROSTER_UPDATE")
+  EPGP:GetModule("guild_info").RegisterMessage(
+    self, "BaseGPChanged", "GUILD_ROSTER_UPDATE")
+end
+
+function mod:OnModuleDisable()
+  EPGP:GetModule("guild_info").UnregisterAllMessages(self)
+end
+
+--------------------------------------------------------------------------------
+-- These functions change the core interface.
+
+function EPGP:StandingsSortOrder()
+  return mod.db.profile.sort_order
+end
+
+function EPGP:SortStandings(order)
+  assert(comparator[order], "Unknown sort order")
+  mod.db.profile.sort_order = order
+  table.sort(standings, comparator[order])
+  mod:SendMessage("StandingsUpdate")
+end
+
+function EPGP:GetNumStandingsMembers()
+  return #standings
+end
+
+function EPGP:GetStandingsInfo(i)
+  return standings[i]
+end
